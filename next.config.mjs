ef4c5cd29dbd8bc323ceb9d1db3cf5f13bdd9d90
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2024-02-29 11:46:33 -0500

    Render basic shader material in full screen

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.glsl/,
+      type: 'asset/source',
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
